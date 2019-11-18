2557a29b5c6d45d8ef0892e98aaa92d02d78ec68
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-11-19 00:18:37 +0700

    Add axios and send to backend.

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -2,3 +2,4 @@ import { BASE_URL } from './../config/config.js'
 
 export const LOGIN_URL = BASE_URL + '/wsbank/login?wsdl'
 export const INFO_URL = BASE_URL + '/wsbank/info?wsdl'
+export const TRANSFER_URL = BASE_URL + '/wsbank/transfer?wsdl'
