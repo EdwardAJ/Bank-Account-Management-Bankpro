6b68c262874daa53718309c0b7b0a0b400caf5de
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-11-21 21:24:49 +0700

    Add amount param

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://localhost:8081";
-export const BASE_URL = "http://engima.club:8081";
+export const BASE_URL = "http://localhost:8081";
+// export const BASE_URL = "http://engima.club:8081";
